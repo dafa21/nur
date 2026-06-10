07516ec6297eaf48bf0c6e1d1701560a14b2ab80
dafa21 <dafa21@users.noreply.github.com>
2026-06-10 15:21:30 +0700

    fix: change pm2 script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,9 +4,8 @@ module.exports = {
       name: 'nurhealth-main',
       // Gunakan file JS yang sudah di-compile (bukan tsx runtime compiler!)
       // 'npx tsx' = TypeScript compiler jalan terus → CPU 100%
-      // 'node dist/server.cjs' = binary JS biasa → CPU normal
-      script: 'node',
-      args: 'dist/server.cjs',
+      // 'dist/server.cjs' = binary JS biasa → CPU normal
+      script: 'dist/server.cjs',
       instances: 1,
       exec_mode: 'fork',
       max_memory_restart: '400M',  // restart otomatis jika RAM > 400MB
